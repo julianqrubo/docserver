47e74929a8eb748ecc0e10fca9e55c8a27c90c37
shareppy <shareppy@jcurubo>
2016-05-22 13:10:26 -0500

    Se sacan en variables los parámetros de conexión de bd

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=docserverjm;charset=utf8', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $database_ip = '192.168.1.37';
+    $database_name = 'docserverjm';
+    $database_pwd = 'qwerty';
+    $db = new PDO('mysql:host='.$database_ip.';dbname='.$database_name.';charset=utf8', 'root', $database_pwd);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
