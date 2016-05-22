b06f0ceb8b83728838ac590c5d2a953000841fba
mcasallas <mcasallas@192.168.1.35>
2016-05-21 22:21:54 -0500

    Se mejora la pantalla de ver los archivos.

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
 //Para ponerlo en producción debe poner $mode = ".min";
 $mode = "";
 
-    $userid = $_SESSION["__user__"];
-    $name = $_SESSION['__name__'];
-    $lastname = $_SESSION['__lastname__'];
-    $fullname = $name." ".$lastname;
-
+$userid = $_SESSION["__user__"];
+$name = $_SESSION['__name__'];
+$lastname = $_SESSION['__lastname__'];
+$isadmin = $_SESSION['__isadmin__'];
+$fullname = $name . " " . $lastname;
 ?>
 
 <!doctype html>
@@ -43,26 +43,30 @@ $mode = "";
 
     <body>
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-  <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <!-- Title -->
-      <span class="mdl-layout-title"><?php echo $fullname; ?></span>
-      <!-- Add spacer, to align navigation to the right -->
-      <div class="mdl-layout-spacer"></div>
-      <!-- Navigation. We hide it in small screens. -->
-      <nav class="mdl-navigation mdl-layout--large-screen-only">
-        <a class="mdl-navigation__link" href="#" id="change-pwd">Cambiar contrase&ntilde;a</a>
-        <a class="mdl-navigation__link" href="logout.php">Cerrar sesi&oacute;n</a>
-      </nav>
-    </div>
-  </header>
-  <div class="mdl-layout__drawer">
-    <span class="mdl-layout-title"><?php echo $fullname; ?></span>
-    <nav class="mdl-navigation">
-      <a class="mdl-navigation__link" href="">Empresas</a>
-      <a class="mdl-navigation__link" href="">Usuarios</a>
-      <a class="mdl-navigation__link" href="">Archivos</a>
-    </nav>
-  </div>
-  <main class="mdl-layout__content">
-    <div class="page-content">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <!-- Title -->
+                    <span class="mdl-layout-title"><?php echo $fullname; ?></span>
+                    <!-- Add spacer, to align navigation to the right -->
+                    <div class="mdl-layout-spacer"></div>
+                    <!-- Navigation. We hide it in small screens. -->
+                    <nav class="mdl-navigation mdl-layout--large-screen-only">
+                        <a class="mdl-navigation__link" href="#" id="change-pwd">Cambiar contrase&ntilde;a</a>
+                        <a class="mdl-navigation__link" href="logout.php">Cerrar sesi&oacute;n</a>
+                    </nav>
+                </div>
+            </header>
+            <div class="mdl-layout__drawer">
+                <span class="mdl-layout-title"><?php echo $fullname; ?></span>
+                <nav class="mdl-navigation">
+                    <?php if ($isadmin) { ?>
+                        <a class="mdl-navigation__link" href="companies.php">Empresas</a>
+                        <a class="mdl-navigation__link" href="users.php">Usuarios</a>
+                        <a class="mdl-navigation__link" href="admfiles.php">Archivos</a>
+                    <?php } else { ?>
+                        <a class="mdl-navigation__link" href="files.php">Archivos</a>
+                    <?php } ?>
+                </nav>
+            </div>
+            <main class="mdl-layout__content">
+                <div class="page-content">
