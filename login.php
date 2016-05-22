516bf5e3c85b6b8919265c708c46448cd8eac3e0
mcasallas <mcasallas@192.168.1.35>
2016-05-21 19:00:02 -0500

    Se agrega soporte de consultas.

--- login.php
+++ login.php
@@ -1,5 +1,20 @@
 <?php
+
+include './db.php';
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+
+$stmt = $db->prepare("SELECT id FROM users WHERE username = ? AND pwd = ?");
+$stmt->execute(array($username, $password));
+$row_count = $stmt->rowCount();
+
+if ($row_count > 0) {
+    echo "1";
+} else {
     http_response_code(401);
     echo "Usuario o contraseña incorrectos.";
+}
 ?>
 
