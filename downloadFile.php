44d8a7744de0130a8b5d091eafcc41b8983e4045
shareppy <shareppy@jcurubo>
2016-05-29 19:53:45 -0500

    Descarga de archivos

--- downloadFile.php
+++ downloadFile.php
@@ -1,4 +1,12 @@
 <?php
-    echo "Descargando archivo";
-?>
 
+session_start();
+echo "Descargando archivo____";
+$path_file_srv = $_GET['path'];
+echo $path_file_srv."_____";
+$filename = $_GET['filename'];
+echo $filename."_____";
+header("Content-type: application/octet-stream"); 
+header("Content-Type: application/force-download"); 
+header("Content-Disposition: attachment; filename=\"$filename\"\n"); readfile($path_file_srv);
+?>
--- files.php
+++ files.php
@@ -24,25 +24,23 @@ if ($row) {
             <ul class="files mdl-list">
                 <?php
                 foreach ($files as $file) {
-                    $uploadId = current(explode('.', $file));
-                    $arrayUploadId[] = $uploadId;
+                    $arrayFile[] = $file;
                 }
-                array_multisort($arrayUploadId, SORT_DESC, SORT_NUMERIC);
-                foreach ($arrayUploadId as $ids) {
+                array_multisort($arrayFile, SORT_DESC, SORT_STRING);
+                foreach ($arrayFile as $ids) {
+                    $uploadId = current(explode('.', $ids));
                     ?>
                     <li class = "mdl-list__item">
                         <span class = "mdl-list__item-primary-content">
                             <i class = "material-icons">attach_file</i>
-                            <a href = "#" id = "download_text">
-                                <?php
-                                $stmt_file = $db->prepare("SELECT source_name FROM upload_file WHERE id = " . $ids);
-                                $stmt_file->execute();
-                                $row_file = $stmt_file->fetch(PDO::FETCH_ASSOC);
-                                echo $row_file['source_name'];
-                                ?>
-                            </a>
+                            <?php
+                            $stmt_file = $db->prepare("SELECT source_name FROM upload_file WHERE id = " . $uploadId);
+                            $stmt_file->execute();
+                            $row_file = $stmt_file->fetch(PDO::FETCH_ASSOC);
+                            ?>
+                            <a href = "downloadFile.php?path=<?php echo $path . "/" . $ids; ?>&filename=<?php echo $row_file['source_name']; ?>" id = "download_text"><?php echo $row_file['source_name']; ?></a>
                         </span>
-                        <a href="#" id="download_icon"><i class="material-icons">cloud_download</i></a>
+                        <a href="downloadFile.php?path=<?php echo $path . "/" . $ids; ?>&filename=<?php echo $row_file['source_name']; ?>" id="download_icon"><i class="material-icons">cloud_download</i></a>
                     </li>
                     <?php
                 }
